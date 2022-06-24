7900135947484cc7aae189aa3df0807446cdfdc1
Gabriel <bielsantossk8@gmail.com>
2022-06-24 13:20:48 -0300

    algo

---
+++ splash.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Splash Screen</title>
+</head>
+
+<body>
+    <div class="logoempresa">
+        <img class="imglogo" src="./img/logoempresa.png" alt="">
+    </div>
+
+    <div class="descendo">
+        <div class="alinhando">
+            <h4>Powered By </h4>
+        </div>
+        <div class="alinhando">
+            <h4>BILAO</h4>
+        </div>
+    </div>
+</body>
+
+</html>
+
