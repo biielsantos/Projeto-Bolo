75badd4bb614ab003e6bc8702225c33a83016ff7
Gabriel <bielsantossk8@gmail.com>
2022-06-26 15:07:46 -0300

    Criação da pagina de signup

---
+++ signup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="style2.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp</title>
+</head>
+
+<body>
+    <div>
+        <img class="imglogo" src="logo.png">
+    </div>
+    <div class="button-mom">
+        
+        <button class="btn-google" type="button" ><img class="icongoogle" src="img/googleimg.png"><h4 class="h4btn1">Entrar com o Google</h4></button>
+
+        <button class="btn-facebook" type="button"><img class="iconfacebook" src="img/facebookimg.png"><h4 class="h4btn2">Entrar com o Facebook</h4></button>
+    </div>
+
+    <h4 class="h4centro"> Ou crie sua conta</h4>
+
+</body>
+
+</html>
