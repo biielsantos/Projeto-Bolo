ccd5c75c1b0fc5a73b6f7bb1372bd2e657330676
Gabriel <bielsantossk8@gmail.com>
2022-06-26 19:58:35 -0300

    Atualização da pagina SignUp

--- signup.php
+++ signup.php
@@ -11,18 +11,36 @@
 </head>
 
 <body>
+    <div class="divarrow"><img class="imgarrow" src="img/arrow-left.png"></div>
+    
     <div>
         <img class="imglogo" src="logo.png">
     </div>
     <div class="button-mom">
-        
-        <button class="btn-google" type="button" ><img class="icongoogle" src="img/googleimg.png"><h4 class="h4btn1">Entrar com o Google</h4></button>
 
-        <button class="btn-facebook" type="button"><img class="iconfacebook" src="img/facebookimg.png"><h4 class="h4btn2">Entrar com o Facebook</h4></button>
+        <button class="btn-google" type="button"><img class="icongoogle" src="img/googleimg.png">
+            <h4 class="h4btn1">Entrar com o Google</h4>
+        </button>
+
+        <button class="btn-facebook" type="button"><img class="iconfacebook" src="img/facebookimg.png">
+            <h4 class="h4btn2">Entrar com o Facebook</h4>
+        </button>
     </div>
 
     <h4 class="h4centro"> Ou crie sua conta</h4>
 
+    <div class="cadastropai">
+
+        <input class="inptcadastro" id="placeteste" placeholder="Nome e Sobrenome"></input>
+        <input class="inptcadastro" placeholder="Endereço de Email"></input>
+        <input class="inptcadastro" placeholder="Telefone ou Celular"></input>
+        <input class="inptcadastro" placeholder="Senha" type="password"></input>
+        <input class="inptcadastro" placeholder="Confirmar Senha" type="password"></input>
+
+    </div>
+    <div class="button-mom2">
+    <button class="btnregistro" type="button">Registrar-se</button>
+</div>
 </body>
 
 </html>
