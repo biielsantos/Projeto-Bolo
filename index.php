891256d61b6d46b960f1691f0232b3498d31e2c1
Gabriel <bielsantossk8@gmail.com>
2022-06-28 18:14:29 -0300

    Subindo index

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="style2.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cleusa Lindes bolos</title>
 </head>
+
 <body>
-    <div>
-        <h1></h1>
+    <div class="cima">
+        <div class="divflex"><img class="imglateral" src="img/imglateral.png"></div>
+        <div class="divflex"><img class="imglogo" src="logo.png"></div>
+        <div class="divflex"><img class="imgcarrinho" src="img/imgcarrinho.png"></div>
     </div>
 </body>
+
+
 </html>
